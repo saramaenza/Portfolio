5bf5f9a204d4917dbd1e145ba23f1fa6cda78be3
SaraMaenza <sara.maenza98@gmail.com>
2026-01-04 15:48:38 +0100

    Add Cards component

---
+++ src/components/Cards.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import Card from './Card';
+
+function importAll(r) {
+  let images = {};
+  r.keys().forEach((item) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
+
+function Cards({ skills }) {
+  return (
+    <div className="mt-5 flex flex-wrap justify-center px-20">
+      {skills.map((skill) => (
+        <Card key={skill.id} skill={skill} image={images[skill.img]} />
+      ))}
+    </div>
+  );
+}
+
+Cards.propTypes = {
+  skills: PropTypes.arrayOf(PropTypes.object),
+};
+
+Cards.defaultProps = {
+  skills: [],
+};
+
+export default Cards;
