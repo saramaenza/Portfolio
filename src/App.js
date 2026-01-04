d2b7fd398fe90524c30fc8705103d3a3d0d7bc2d
SaraMaenza <sara.maenza98@gmail.com>
2026-01-04 15:45:05 +0100

    Add components in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React from 'react';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
+  React.useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App scroll-smooth">
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Footer></Footer>
     </div>
   );
 }
