e9a4f10320f7ffa265466f3beda9225d5bc211dc
SaraMaenza <sara.maenza98@gmail.com>
2026-01-04 15:48:19 +0100

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <section
+        className="relative isolate overflow-hidden w-full flex items-center justify-center"
+    >
+        <div className="relative pt-20 pb-10 w-full px-15">
+            <div className="w-full flex justify-center" data-aos="fade-up" data-aos-delay="300">
+                <p className="text-neutral-600 text-center text-md font-medium font-quicksand">
+                    Designed & Developed by <span className="text-carnation">Sara Maenza</span>
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Footer;
